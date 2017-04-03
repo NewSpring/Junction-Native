daa6255aa5a55a1f23294cb9771d0bec868b702c
Jake Dawkins <dawkinsjh@gmail.com>
2017-04-03 16:39:02 -0400

    added rounded helper

---
+++ src/helpers/__tests__/rounded.js
@@ -0,0 +1,26 @@
+import Style from "further";
+import rounded from "../rounded";
+
+const props = { theme: { baseSize: 2 } };
+
+describe("rounded", () => {
+  it("modifies border radius at top level", () => {
+    expect(rounded.resolve(props)).toEqual({ borderRadius: 4 });
+  });
+  it("modifies border for a specific direction", () => {
+    expect(rounded.topLeft.resolve(props)).toEqual({
+      borderTopLeftRadius: 4,
+    });
+  });
+  it("modifies border with direction and size", () => {
+    expect(rounded.bottomRight.sm.resolve(props)).toEqual({
+      borderBottomRightRadius: 2,
+    });
+    expect(rounded.bottomRight.xl.resolve(props)).toEqual({
+      borderBottomRightRadius: 16,
+    });
+  });
+  it("modifies border with size modifier only", () => {
+    expect(rounded.lg.resolve(props)).toEqual({ borderRadius: 8 });
+  });
+});
---
+++ src/helpers/rounded.js
@@ -0,0 +1,52 @@
+//@flow
+import cap from "./util/upperCaseFirstLetter";
+import Style from "further";
+import lens from "./util/lenses/baseSize";
+
+const getRadius = props => lens("radius", props).radius;
+
+const directions = [
+  "topLeft",
+  "topRight",
+  "bottomLeft",
+  "bottomRight",
+  "top",
+  "bottom",
+  "left",
+  "right",
+  "horizontal",
+  "vertical",
+];
+
+const sizes = {
+  none: () => 0,
+  xs: x => x / 4,
+  sm: x => x / 2,
+  md: x => x,
+  lg: x => x * 2,
+  xl: x => x * 4,
+};
+sizeKeys = Object.keys(sizes);
+
+const rounded = Style(props => ({
+  borderRadius: getRadius(props),
+}));
+
+directions.map(direction => {
+  const directionKey = `border${cap(direction)}Radius`;
+  rounded[direction] = Style(props => ({ [directionKey]: getRadius(props) }));
+  sizeKeys.map(
+    sizeKey =>
+      rounded[direction][sizeKey] = Style(props => ({
+        [directionKey]: sizes[sizeKey](getRadius(props)),
+      })),
+  );
+});
+
+sizeKeys.map(sizeKey => {
+  rounded[sizeKey] = Style(props => ({
+    borderRadius: sizes[sizeKey](getRadius(props)),
+  }));
+});
+
+export default rounded;
